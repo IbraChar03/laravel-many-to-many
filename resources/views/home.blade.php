ac7c71c75e0a48f6be577b3a08cf6f1b1b97660b
Ibrahim <ibrachar68@gmail.com>
2023-02-14 15:01:40 +0100

    stampa in pagine dei prodotti divisi in categorie

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.main-layout')
 @section('content')
-ciao
+<h1>CATEGORIES AND PRODUCTS</h1>
+<ul>
+    @foreach ($categories as $category)
+    <h1>{{$category -> name}}</h1>
+    @foreach ($category -> products as $product)
+    <li>
+        <strong>Product Name : </strong> {{$product -> name}} -- <strong>Digital : </strong> {{$product -> typology ->
+        digital ? "Yes" : "No"}}
+    </li>
+    @endforeach
+    <hr>
+    @endforeach
+</ul>
 @endsection

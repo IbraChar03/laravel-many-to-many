78164f5eff0155f22c11e8874f5e0c09bdeff600
Ibrahim <ibrachar68@gmail.com>
2023-02-14 15:09:48 +0100

    stampa dati pagina products

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,6 +4,11 @@
 <ul>
     @foreach ($products as $product)
     <h1>{{$product -> name}}</h1>
+    <strong>Description : </strong> {{$product -> description}} <br>
+    <strong>Price : </strong> {{$product -> price}} <br>
+    <strong>Weight : </strong> {{$product -> weight}} <br>
+    <strong>Typology : </strong> {{$product -> typology -> name}} <br>
+    <strong>Digital : </strong> {{$product -> typology -> digital ? "Yes" : "No"}} <br>
     <hr>
     @endforeach
 </ul>
